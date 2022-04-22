bd05be53666d09ecf46776db40c47dbef094ddf5
Mike Reimer <themichaelreimer@gmail.com>
2022-04-22 14:57:05 -0500

    Add MIT licence

--- regexp.py
+++ regexp.py
@@ -1,3 +1,14 @@
+"""
+Copyright 2022 Mike Reimer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
 import re
 from typing import Union
 
@@ -97,4 +108,4 @@ print(f'{eval("1+(2*3)")=}')
 print(f'{eval("3-4/2^2")=}')
 print(f'{eval("-1*(2-3)")=}')
 print(f'{eval(":)")=}')
-print(f'{eval("(1+(1+(1+1)))+(1+(1+1)+1) ")=}')  # TODO: This one doesn't quite work, investigate
+print(f'{eval("(1+(1+(1+1)))+(1+(1+1)+1) ")=}') 
