deef5bbeeb4ae2d1daba9421f926f75d7a32ca49
Mike Reimer <themichaelreimer@gmail.com>
2022-04-29 12:49:14 -0500

    Add decimal support, fix small bug

--- regexp.py
+++ regexp.py
@@ -1,30 +1,29 @@
 """
+regexp.py https://github.com/Themichaelreimer/regex-math-parser
+
 Copyright 2022 Mike Reimer
-
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-
 import re
 from typing import Union
+from decimal import Decimal
 
 binary_operators = [r'\^', r'\*', r'\/', r'\+', r'\-']
 
-digit_pattern = r'\-?\d+'
+digit_pattern = r'\-?\d+\.?\d*'
 operator_pattern = r'[{}]'.format(
     ''.join(r'{}'.format(op) for op in binary_operators))  # When I print this the chars look double escaped
 operation_pattern = r'({0})({1})({0})'.format(digit_pattern, operator_pattern)
 bracket_pattern = r'(\({}\))'.format(operation_pattern)
 
-binary_patterns = [r'({0})({1})({0})'.format(digit_pattern, op) for op in binary_operators]
+binary_patterns = [r'({0})\s?({1})\s?({0})'.format(digit_pattern, op) for op in binary_operators]
 patterns = [bracket_pattern, *binary_patterns]  # Each pattern in this list is in BEDMAS and in BEDMAS order
 
 
-def eval(input: str) -> Union[int,str]:
+def eval(input: str) -> Union[Decimal,str]:
     """
     Returns an integer representing the result in an integer-only number system, or the string "ERROR"
     :param input:
@@ -59,53 +58,30 @@ def eval(input: str) -> Union[int,str]:
 
     result = re.search(digit_pattern, input)
     if result:
-        return int(result.group(0))
+        return Decimal(result.group(0))
     else:
         return "ERROR"
 
 
-def brackets(input: str) -> int:
+def brackets(input: str) -> Decimal:
     return eval(input[1:-1])
 
 
-def exp(a: str, b: str) -> int:
-    return int(a) ** int(b)
+def exp(a: str, b: str) -> Decimal:
+    return Decimal(a) ** Decimal(b)
 
 
-def mul(a: str, b: str) -> int:
-    return int(a) * int(b)
+def mul(a: str, b: str) -> Decimal:
+    return Decimal(a) * Decimal(b)
 
 
-def div(a: str, b: str) -> int:
-    return int(int(a) / int(b))
+def div(a: str, b: str) -> Decimal:
+    return Decimal(a) / Decimal(b)
 
 
-def brackets(input: str) -> int:
-    return eval(input[1:-1])
+def add(a: str, b: str) -> Decimal:
+    return Decimal(a) + Decimal(b)
 
 
-def exp(a: str, b: str) -> int:
-    return int(a) ** int(b)
-
-
-def mul(a: str, b: str) -> int:
-    return int(a) * int(b)
-
-
-def div(a: str, b: str) -> int:
-    return int(a) / int(b)
-
-
-def add(a: str, b: str) -> int:
-    return int(a) + int(b)
-
-
-def sub(a: str, b: str) -> int:
-    return int(a) - int(b)
-
-# Sample inputs
-print(f'{eval("1+(2*3)")=}')
-print(f'{eval("3-4/2^2")=}')
-print(f'{eval("-1*(2-3)")=}')
-print(f'{eval(":)")=}')
-print(f'{eval("(1+(1+(1+1)))+(1+(1+1)+1) ")=}') 
+def sub(a: str, b: str) -> Decimal:
+    return Decimal(a) - Decimal(b)
